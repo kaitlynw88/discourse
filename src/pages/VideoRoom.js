11b030a8591dd5a66d806ce517b2ae7cc3313edd
KaitlynWickson <kwickson28@gmail.com>
2023-01-20 17:58:59 -0500

    audio working

--- src/pages/VideoRoom.js
+++ src/pages/VideoRoom.js
@@ -1,6 +1,6 @@
 import "../styles/videoRoom.scss";
 import { Link } from "react-router-dom";
-import AgoraRTC from "agora-rtc-sdk-ng";
+import AgoraRTC, {createClient} from "agora-rtc-sdk-ng";
 import { useEffect, useState } from "react";
 //  
 import AudioPlayer from "../components/AudioPlayer";
@@ -9,49 +9,125 @@ const TOKEN =
     "007eJxTYNDiXPdj6rLfUf/+aV1S6r7DqTOxj23lus3ML7a90t9npzNNgSHZwDI5JdXQxNLANA1ImFmaGialJSWbpqSmJqUZJVm8+X0quSGQkeGO20xWRgYIBPE5GVIyi5PzS4uKUxkYAKrfJLQ=";
 const CHANNEL = "discourse"
 
-const client = AgoraRTC.createClient({
-    mode: "rtc",
-    codec: "vp8"
-}) 
- 
+AgoraRTC.setLogLevel(4);
+
+let agoraCommandQueue = Promise.resolve();
+
+const createAgoraClient = ({ onVideoTrack, onUserDisconnected }) => {
+    const client = createClient({
+        mode: "rtc",
+        codec: "vp8",
+    });
+
+    let tracks;
+
+    const waitForConnectionState = (connectionState) => {
+        return new Promise((resolve) => {
+            const interval = setInterval(() => {
+                if (client.connectionState === connectionState) {
+                    clearInterval(interval);
+                    resolve();
+                }
+            }, 200);
+        });
+    };
+
+    const connect = async () => {
+        await waitForConnectionState("DISCONNECTED");
+
+        const uid = await client.join(APP_ID, CHANNEL, TOKEN, null);
+
+        client.on("user-published", (user, mediaType) => {
+            client.subscribe(user, mediaType).then(() => {
+                if (mediaType === "video") {
+                    onVideoTrack(user);
+                }
+                if (mediaType === "audio") {
+                    user.audioTrack.play();
+                }
+            });
+        });
+
+        client.on("user-left", (user) => {
+            onUserDisconnected(user);
+        });
+
+        tracks = await AgoraRTC.createMicrophoneAndCameraTracks();
+
+        await client.publish(tracks);
+
+        return {
+            tracks,
+            uid,
+        };
+    };
+
+    const disconnect = async () => {
+        await waitForConnectionState("CONNECTED");
+        client.removeAllListeners();
+        for (let track of tracks) {
+            track.stop();
+            track.close();
+        }
+        await client.unpublish(tracks);
+        await client.leave();
+    };
+
+    return {
+        disconnect,
+        connect,
+    };
+};
+
+   
 
 const VideoRoom =()=>{
     const [users, setUsers]=useState([])
-    const handleUserJoined=async(user, mediaType)=>{
-        await client.subscribe(user, mediaType);
-        
-        if (mediaType === "video") {
-            setUsers((previousUsers)=> [...previousUsers, user])
-        }
+    const [uid, setUid]=useState(null)
 
-        if(mediaType === "audio"){
-            // user.audioTrack.play()
-        }
+    useEffect(() => {
+        const onVideoTrack = (user) => {
+            setUsers((previousUsers) => [...previousUsers, user]);
+        };
 
-    }
+        const onUserDisconnected = (user) => {
+            setUsers((previousUsers) =>
+                previousUsers.filter((u) => u.uid !== user.uid)
+            );
+        };
 
-    const handleUserLeft =(user)=>{
-        setUsers((previousUsers)=>
-        previousUsers.filter((u) => u.uid !== user.uid))
-    }
+        const { connect, disconnect } = createAgoraClient({
+            onVideoTrack,
+            onUserDisconnected,
+        });
 
-    useEffect(()=>{
-        client.on("user-published", handleUserJoined)
-        client.on("user-left", handleUserLeft);
+        const setup = async () => {
+            const { tracks, uid } = await connect();
+            setUid(uid);
+            setUsers((previousUsers) => [
+                ...previousUsers,
+                {
+                    uid,
+                    audioTrack: tracks[0],
+                    videoTrack: tracks[1],
+                },
+            ]);
+        };
 
-        client.join(APP_ID, CHANNEL, TOKEN, null)
-        .then((uid)=>
-            Promise.all([AgoraRTC.createMicrophoneAndCameraTracks(),uid])
-        ).then(([tracks, uid]) =>{
-            const [audioTrack, videoTrack]=tracks;
-            setUsers((previousUsers)=> [...previousUsers, {
-                uid,
-                videoTrack,
-                audioTrack 
-            }])
-            client.publish(tracks)
-        })
-    },[])
+        const cleanup = async () => {
+            await disconnect();
+            setUid(null);
+            setUsers([]);
+        };
+
+        // setup();
+        agoraCommandQueue = agoraCommandQueue.then(setup);
+
+        return () => {
+            // cleanup();
+            agoraCommandQueue = agoraCommandQueue.then(cleanup);
+        };
+    }, []);
     
     return (
         <>
@@ -59,7 +135,7 @@ const VideoRoom =()=>{
             <div className="roomContainer">
                 
                 <ul className="chatLog">
-                    <h3>Chat log</h3>
+                    <h3>Chat log</h3> 
                     <li>
                         <p>username1</p>
                         <p>
@@ -86,12 +162,12 @@ const VideoRoom =()=>{
                     </li>
                 </ul>
 
-                {users.map((user)=>{
+                {users.map((user)=>{ 
                     return(
+
                         <div>
-                                {user.uid}
-                            <AudioPlayer key={user.uid} user={user}>
-                            </AudioPlayer>
+                            {uid}
+                            <AudioPlayer key={user.uid} user={user}/>
                         </div>
 
                     )
