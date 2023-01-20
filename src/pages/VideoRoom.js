954c65f7f112d5ef3688b32c6f3ea05c97749c50
KaitlynWickson <kwickson28@gmail.com>
2023-01-20 16:45:44 -0500

    audio

--- src/pages/VideoRoom.js
+++ src/pages/VideoRoom.js
@@ -46,7 +46,8 @@ const VideoRoom =()=>{
             const [audioTrack, videoTrack]=tracks;
             setUsers((previousUsers)=> [...previousUsers, {
                 uid,
-                videoTrack 
+                videoTrack,
+                audioTrack 
             }])
             client.publish(tracks)
         })
