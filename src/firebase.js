439f282293f0bacf8325a0b577897005b61a9268
GedeonTS <gedeontshobohwa1@gmail.com>
2023-02-04 13:40:28 +0200

    Create connection to Realtime Database

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import {getFirestore} from "firebase/firestore"
+import { getFirestore } from "firebase/firestore"
+import { getDatabase } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -27,7 +28,10 @@ export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 
 //connect to firestore
-export const db=getFirestore(app)
+export const db = getFirestore(app)
+
+//connect to realtime database
+export const RealtimeDatabase = getDatabase(app)
 
 export default app;
 
