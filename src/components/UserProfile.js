c3a5594307f3fd3a947fd0ae08020e385d270a8d
Ushindi Gedeon <gedeontshobohwa1@gmail.com>
2023-02-07 20:04:57 +0200

    Add user profile imge to profile page

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,32 +1,41 @@
+import { Avatar } from "@mui/material";
 import { Link } from "react-router-dom";
-import "../styles/userprofile.scss"
+import "../styles/userprofile.scss";
 
 const UserProfile = (props) => {
-    
-    return (
-        <div className="userProfile">
-            {props.profile ? (
-                <>
-                    <h3 className="userName">
-                        {props.profile.firstName} {props.profile.lastName}
-                    </h3>
-                    <div>
-                        <p>{props.profile.bio}</p>
-                    </div>
-                </>
-            ) : (
-                <>
-                    <div>
-                        <p>please set up your profile here</p>
-                        
-                        <button>
-                            <Link to="/ProfileForm">profile setup</Link>
-                        </button>
-                    </div>
-                </>
-            )}
-        </div>
-    );
+  return (
+    <div className="userProfile">
+      {props.profile ? (
+        <>
+          {props.profile.avatar ? (
+            <Avatar
+              alt="Remy Sharp"
+              src={props.profile.avatar}
+              sx={{ width: 156, height: 156 }}
+            />
+          ) : (
+            ""
+          )}
+          <h3 className="userName">
+            {props.profile.firstName} {props.profile.lastName}
+          </h3>
+          <div>
+            <p>{props.profile.bio}</p>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <p>please set up your profile here</p>
+
+            <button>
+              <Link to="/ProfileForm">profile setup</Link>
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default UserProfile;
